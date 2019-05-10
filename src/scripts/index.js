b17c5bcc063a93c0987433e32470e5842ccb51b8
Jack Rugile <jackrugile@gmail.com>
2019-05-10 02:23:58 -0600

    start to break apart css into sections

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,13 @@
-let header = document.querySelector('header');
-let scrolled = 0;
+// let header = document.querySelector('header');
+// let scrolled = 0;
 
-window.addEventListener('scroll', () => {
-  scrolled = window.scrollY;
-});
+// window.addEventListener('scroll', () => {
+//   scrolled = window.scrollY;
+// });
 
-function loop() {
-  window.requestAnimationFrame(loop);
-  header.style.transform = `translate3d(0, ${scrolled * 0.5}px, 0)`;
-}
+// function loop() {
+//   window.requestAnimationFrame(loop);
+//   header.style.transform = `translate3d(0, ${scrolled * 0.5}px, 0)`;
+// }
 
-loop();
+// loop();
