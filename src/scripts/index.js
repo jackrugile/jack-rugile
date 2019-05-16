95e37949e118bf2ce8b05552f104705497d9e608
Jack Rugile <jackrugile@gmail.com>
2019-05-15 20:53:40 -0600

    add more projects, add call to action section, start styling footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
 import 'lazysizes';
 
-// let header = document.querySelector('header');
+// function map(val, inputMin, inputMax, outputMin, outputMax) {
+//  return ((outputMax - outputMin) * ((val - inputMin) / (inputMax - inputMin))) + outputMin;
+// }
+
+// function clamp(val, min, max) {
+//    return Math.max(Math.min(val, max), min);
+//  }
+
+// let headerTitle = document.querySelector('.header-title');
 // let scrolled = 0;
 
 // window.addEventListener('scroll', () => {
@@ -9,7 +17,17 @@ import 'lazysizes';
 
 // function loop() {
 //   window.requestAnimationFrame(loop);
-//   header.style.transform = `translate3d(0, ${scrolled * 0.5}px, 0)`;
+
+//   let y = scrolled * 0.5;
+
+//   let opacity = map(scrolled, 200, 400, 1, 0);
+//   opacity = clamp(opacity, 0, 1);
+
+//   let scale = map(scrolled, 200, 400, 1, 0.9);
+//   scale = clamp(scale, 0, 1);
+
+//   headerTitle.style.transform = `translate3d(0, ${y}px, 0) scale(${scale})`;
+//   headerTitle.style.opacity = opacity;
 // }
 
 // loop();
