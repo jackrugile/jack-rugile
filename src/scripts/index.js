22bf2d8caef3ba6adab7336e2960e3659000c6c4
Jack Rugile <jackrugile@gmail.com>
2019-05-18 00:49:43 -0600

    style 404 page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,3 +17,19 @@ if (
 document.addEventListener('lazyloaded', function(e) {
   e.target.parentNode.classList.add('image-loaded');
 });
+
+let is404 = document.querySelector('.is-404');
+
+function set404Height() {
+  document.documentElement.style.height = `${window.innerHeight}px`;
+  document.body.style.height = `${window.innerHeight}px`;
+
+  document.documentElement.style.minHeight = `0px`;
+  document.body.style.minHeight = `0px`;
+}
+
+if(is404) {
+  set404Height();
+  window.addEventListener('resize', set404Height);
+  window.addEventListener('orientationchange', set404Height);
+}
