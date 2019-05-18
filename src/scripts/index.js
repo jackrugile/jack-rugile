46c1af220c64075ce33b251e670d985cf232567c
Jack Rugile <jackrugile@gmail.com>
2019-05-17 22:59:39 -0600

    fix angle subpixel issues, fix mac safari body bg color,

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,13 @@
 import 'lazysizes';
 
 lazySizes.cfg.expand = 1000;
+
+if (
+  navigator.userAgent.indexOf('Safari') != -1 &&
+  navigator.userAgent.indexOf('Mac') != -1 &&
+  navigator.userAgent.indexOf('Chrome') == -1 &&
+  navigator.userAgent.indexOf('iPad') == -1 &&
+  navigator.userAgent.indexOf('iPhone') == -1
+  ) {
+  document.documentElement.classList.add('mac-os');
+}
