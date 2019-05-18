a6ef811bfa03ec180fe204da307eae7c08c7de84
Jack Rugile <jackrugile@gmail.com>
2019-05-18 00:02:10 -0600

    add loader spinner for images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,8 @@ import 'lazysizes';
 
 lazySizes.cfg.expand = 1000;
 
+document.documentElement.classList.remove('no-js');
+
 if (
   navigator.userAgent.indexOf('Safari') != -1 &&
   navigator.userAgent.indexOf('Mac') != -1 &&
@@ -11,3 +13,7 @@ if (
   ) {
   document.documentElement.classList.add('mac-os');
 }
+
+document.addEventListener('lazyloaded', function(e) {
+  e.target.parentNode.classList.add('image-loaded');
+});
